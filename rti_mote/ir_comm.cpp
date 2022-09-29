b4e5e5b73c2f221f21eadbf8059967c0bb66dbaf
Singhanat <dipl.ing.style@gmail.com>
2022-09-29 19:41:50 +0700

    ir 3

--- rti_mote/ir_comm.cpp
+++ rti_mote/ir_comm.cpp
@@ -1,15 +1,35 @@
 #include "ir_comm.h"
 #include <IRremote.hpp>
 
+volatile int* p_write;
+volatile bool sIRRecord = false;
+
+void ARDUINO_ISR_ATTR irRecCB() {
+  if (sIRRecord) {
+    detachInterrupt(digitalPinToInterrupt(IR_RECEIVE_PIN));
+    uint16_t ir = analogRead(IR_RECEIVE_PIN);
+    uint8_t c = 0;
+    while ((ir == 0) && (c++ < 100)) {
+      ir = analogRead(IR_RECEIVE_PIN);
+    }
+    verf("CALLBACK IR Analog Read: %02i..", ir);
+    if (ir) {
+      *p_write = ir;
+      verf("Set IR = %04i \n", ir);
+    }
+    attachInterrupt(IR_RECEIVE_PIN, irRecCB, RISING);
+  }
+}
 void ir_comm::begin(unsigned int txPIN) {
   outln(F("START " __FILE__ " from " __DATE__
           "\r\nUsing library version " VERSION_IRREMOTE));
-  pinMode(IR_RECEIVE_PIN, INPUT);
+  pinMode(IR_RECEIVE_PIN, INPUT_PULLUP);
+  attachInterrupt(IR_RECEIVE_PIN, irRecCB, RISING);
   IrSender.begin(txPIN);
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  // IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
 
-  out(F("Ready to receive IR signals of protocols \n"));
-  printActiveIRProtocols(&Serial);
+  // out(F("Ready to receive IR signals of protocols \n"));
+  // printActiveIRProtocols(&Serial);
 }
 
 void ir_comm::begin() {
@@ -21,36 +41,33 @@ void ir_comm::send() {
   IrSender.sendNEC(IR_ADDRESS_16BIT, DEVICE_ID, IR_REPETITION);
 }
 
-uint16_t ir_comm::rss() {
-  return analogRead(IR_RECEIVE_PIN);
-}
-
 void ir_comm::receive() {
-  if (IrReceiver.decode()) {
-    re("Received IR:");
-    // IrReceiver.printIRResultShort(&Serial);
-    // IrReceiver.printIRSendUsage(&Serial);
-    // // TODO keep value to evaluate RTI
-    // re(IrReceiver.decodedIRData.command);
-    *p_write = 1;
-    reln(":Write IR reception BOOLEAN value");
-    // if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
-    //   Serial.println(
-    //       F("Received noise or an unknown (or not yet enabled) protocol"));
-    //   // We have an unknown protocol here, print more info
-    //   IrReceiver.printIRResultRawFormatted(&Serial, true);
-    // }
-    // Serial.println();
-
-    IrReceiver.resume();  // Enable receiving of the next value
-  }
-  // if (sIRRecord) {
-  //   uint16_t ir = rss();
-  //   verf("IR Analog Read: %02d", ir);
-  //   if (ir) {
-  //     verf("Set IR = %04d \n", ir);
-  //   }
+  // if (IrReceiver.decode()) {
+  // re("Received IR:");
+  // IrReceiver.printIRResultShort(&Serial);
+  // IrReceiver.printIRSendUsage(&Serial);
+  // // TODO keep value to evaluate RTI
+  // re(IrReceiver.decodedIRData.command);
+  // *p_write = 1;
+  // reln(":Write IR reception BOOLEAN value");
+  // if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+  //   Serial.println(
+  //       F("Received noise or an unknown (or not yet enabled) protocol"));
+  //   // We have an unknown protocol here, print more info
+  //   IrReceiver.printIRResultRawFormatted(&Serial, true);
   // }
+  // Serial.println();
+
+  // IrReceiver.resume();  // Enable receiving of the next value
+  // }
+  if (sIRRecord) {
+    uint16_t ir = analogRead(IR_RECEIVE_PIN);
+    verf("IR Analog Read: %02i..", ir);
+    if (ir) {
+      *p_write = ir;
+      verf("Set IR = %04i \n", ir);
+    }
+  }
 }
 
 void ir_comm::set_p_write(volatile int* irRSS) {
@@ -59,4 +76,9 @@ void ir_comm::set_p_write(volatile int* irRSS) {
   sIRRecord = true;
   verln("Set Flag IR reception");
   receive();
+}
+
+void ir_comm::setFlag(bool b, volatile int* p) {
+  sIRRecord = b;
+  p_write = p;
 }
